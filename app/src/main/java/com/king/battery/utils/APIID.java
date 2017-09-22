0d07b8a8efc94034086509ebbc33911b096342bb
zhoukang <zhoukang@yuxun.com.cn>
2017-09-22 14:31:28 +0800

    speed

--- app/src/main/java/com/king/battery/utils/APIID.java
+++ app/src/main/java/com/king/battery/utils/APIID.java
@@ -6,18 +6,18 @@ package com.king.battery.utils;
 
 public interface APIID {
     // yyb
-    String ADAPP = "1101189414";
-    String loadingkey = "9060124323597588";
-    String banner = "5040624571474334";
-    String chaping = "1000129344049991";
-    String nativead = "9080726576764571";
+//    String ADAPP = "1101189414";
+//    String loadingkey = "9060124323597588";
+//    String banner = "5040624571474334";
+//    String chaping = "1000129344049991";
+//    String nativead = "9080726576764571";
 
 
 
     //baidu
-//    String ADAPP = "1106156011";
-//    String loadingkey = "8030326585147706";
-//    String banner = "3070323555242789";
-//    String chaping = "1000129344049991";
-//    String nativead = "9050923506476693";
+    String ADAPP = "1106156011";
+    String loadingkey = "8030326585147706";
+    String banner = "3070323555242789";
+    String chaping = "1000129344049991";
+    String nativead = "9050923506476693";
 }
